a30067145240e66ba309727144eba739a709b84e
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-08-25 19:38:53 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,70 +1,11 @@
+import {aleatorio} from './aleatorio.js';
+import {perguntas} from './perguntas.js';
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
-const perguntas = [
-    {
-        enunciado: "Pergunta 1",
-        alternativas: [
-            {
-                texto: "Alternativa 1 da pergunta 1",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto: "Alternativa 2 da pergunta 1",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }           
-            
-        ]
-    },
-    {
-        enunciado: "Pergunta 2",
-        alternativas: [
-            {
-                texto:"Alternativa 1 da pergunta 2",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto: "Alternativa 2 da pergunta 2",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-        ]
-    },
-    {
-        enunciado: "Pergunta 3",
-        alternativas: [
-            {
-                texto:"Alternativa 1 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto:"Alternativa 2 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-            
-        ]
-    },
-];
 
 let atual = 0; 
 let perguntaAtual;
@@ -101,11 +42,6 @@ function mostraResultado(){
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = ""; 
-}
-
-function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.lenght);
-    return lista[posicao];
 }
 
 mostraPergunta();
