29ddd24c25c5e0bdfbe156431c105df6b0aa6956
thisisjao <128715989+thisisjao@users.noreply.github.com>
2024-09-02 10:14:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+import {aleatorio} from './aleatorio.js';
+import {perguntas} from './perguntas.js';
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+const botaoJogarNovamente = document.querySelector(".novamente"-btn);
 
 const perguntas = [
     {
