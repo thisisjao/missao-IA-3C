1521202788a6261ecf03874ad57c28e2a083f67c
thisisjao <128715989+thisisjao@users.noreply.github.com>
2024-09-11 07:51:53 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,8 @@
+'JUcconst nomes = ["Jão, "BoraBora", "Rondinelli", "Laiane", "Marta", "Mia"];
 
+  export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
