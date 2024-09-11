55096759605712952f13a4f7f9b80ffc04155936
thisisjao <128715989+thisisjao@users.noreply.github.com>
2024-09-11 07:53:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import {aleatorio} from './aleatorio.js';
+import {aleatorio, nome} from './aleatorio.js';
 import {perguntas} from './perguntas.js';
 
 const caixaPrincipal = document.querySelector(".caixa-principal");
