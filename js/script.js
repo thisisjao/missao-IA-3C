461e25f559b009dc003315fddbd1ae0249f2286a
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-09-22 17:15:16 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ function respostaSelecionada(opcaoSelecionada){
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = `Em 2049, &{nome}`/*add 12/09*/
+    caixaPerguntas.textContent = `Em 2049, &{nome}`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
     caixaResultado.classList.add("mostrar"); 
